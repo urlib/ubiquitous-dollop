fbcad923c948376216c051cc5b9327a99a69e5a3
vmlankub <45290401+vmlankub@users.noreply.github.com>
2020-02-14 17:11:56 +0800

    Original path: D:/wwwroot/47.103.204.220/main/vfm-admin/users/users.php

---
+++ 3DbWino6MNLqQX7d/NDcuMTAzLjIwNC4yMjA$/bWFpbg$$/dmZtLWFkbWlu/dXNlcnM$/dXNlcnMucGhw.php
@@ -0,0 +1,218 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'admin',
+    'pass' => '$1$CwwjeESh$ek8EU4/OA5QXnxRfL1syp.',
+    'role' => 'superadmin',
+  ),
+  1 => 
+  array (
+    'name' => 'Algha_Porthos',
+    'pass' => '$1$OXs4a0rZ$ReCt.WMG5gtIx0Jv2aFWX.',
+    'role' => 'superadmin',
+  ),
+  2 => 
+  array (
+    'name' => 'mcfuns',
+    'pass' => '$1$YFTOiYSC$hV/HGDU.OMLfsCMCT01iu0',
+    'role' => 'superadmin',
+    'email' => 'mcfuns@vip.qq.com',
+  ),
+  3 => 
+  array (
+    'name' => 'Bugatti',
+    'pass' => '$1$6mwMuGK0$nnortTJvYjPEc37psq3do0',
+    'role' => 'user',
+  ),
+  4 => 
+  array (
+    'name' => 'fyn040902',
+    'pass' => '$1$o5FCziRy$utd8hgeewZRAeh/iBV3SH1',
+    'role' => 'user',
+    'quota' => '50',
+    'dir' => '["fyn040902"]',
+  ),
+  5 => 
+  array (
+    'name' => 'kingraycao',
+    'pass' => '$1$YtDjLh8m$6l0tpgG3LdzpNvSrbIy4e.',
+    'role' => 'admin',
+    'dir' => '["kingraycao"]',
+  ),
+  6 => 
+  array (
+    'name' => 'Oblivion',
+    'pass' => '$1$JBk9ivlq$hJVmcYB33SjpgC8/kFbrx/',
+    'role' => 'superadmin',
+  ),
+  8 => 
+  array (
+    'name' => 'wy',
+    'pass' => '$1$f9ArsiiT$GYCYrEC58Pb8TaWqouCcR1',
+    'role' => 'admin',
+  ),
+  9 => 
+  array (
+    'name' => '浣熊',
+    'pass' => '$1$iqNul6H0$2gN4R4fso.jL7Ng5TG0BC1',
+    'role' => 'admin',
+    'email' => '1067381324@qq.com',
+  ),
+  10 => 
+  array (
+    'name' => 'zrzluck99',
+    'pass' => '$1$9xdZ20Uc$0AlfI1ycbBj6qbEXbsCZc.',
+    'role' => 'user',
+  ),
+  11 => 
+  array (
+    'name' => 'Atalod',
+    'pass' => '$1$oODqOdcD$AIA.X46zMFEa5xNtsDUGX.',
+    'role' => 'user',
+  ),
+  12 => 
+  array (
+    'name' => 'Ptk',
+    'pass' => '$1$5H8yNyWw$yZS7EZFGPr3NFN04.bMCX.',
+    'role' => 'user',
+  ),
+  13 => 
+  array (
+    'name' => 'Katsura_Hinagiku',
+    'pass' => '$1$4HTiIMwM$3/CIyoLLb8dng8TSa6lTU0',
+    'role' => 'user',
+    'email' => '1360899544@qq.com',
+  ),
+  14 => 
+  array (
+    'name' => 'stepsys',
+    'pass' => '$1$FTYg0UFJ$jh7vIpITaipgfmLBsrKXd0',
+    'role' => 'user',
+  ),
+  15 => 
+  array (
+    'name' => 'Dilemma',
+    'pass' => '$1$j0kz7R1A$/rCcbBWkXADrFIfYRmDbm0',
+    'role' => 'user',
+  ),
+  16 => 
+  array (
+    'name' => 'hxb',
+    'pass' => '$1$3nOlsWLZ$jH7NbgtqqayKmDO0IHaKA0',
+    'role' => 'user',
+  ),
+  17 => 
+  array (
+    'name' => 'Crossing',
+    'pass' => '$1$S05YcsFw$EwedRMJ9jA49QB64UiemA0',
+    'role' => 'user',
+  ),
+  18 => 
+  array (
+    'name' => 'Karnage',
+    'pass' => '$1$MtROVyCy$8BoLAkMzEJDa8aWsoJvL30',
+    'role' => 'user',
+  ),
+  19 => 
+  array (
+    'name' => 'zash',
+    'pass' => '$1$hLs2IQsD$IlDlObKm5q0VDMEoRtyNV/',
+    'role' => 'user',
+  ),
+  20 => 
+  array (
+    'name' => 'aramis',
+    'pass' => '$1$GP59FQFh$mnNmDSJo7qkLhTgB4KQiO/',
+    'role' => 'user',
+  ),
+  21 => 
+  array (
+    'name' => 'mzy2003',
+    'pass' => '$1$9XKilbvu$NiEQlwnhd3gBL3jU9hdqL1',
+    'role' => 'user',
+  ),
+  22 => 
+  array (
+    'name' => 'bjc',
+    'pass' => '$1$FXnx6LmW$IThqXp5p.daAvgqBCiky..',
+    'role' => 'user',
+  ),
+  23 => 
+  array (
+    'name' => '银莲月牙',
+    'pass' => '$1$C9vC1xol$pQKqso11vLIer1SCfidYH0',
+    'role' => 'admin',
+  ),
+  24 => 
+  array (
+    'name' => '白渡昀',
+    'pass' => '$1$HjbfaxYN$gHI124IAg4G4bjBNXtw1B.',
+    'role' => 'user',
+  ),
+  25 => 
+  array (
+    'name' => 'zyy_123456',
+    'pass' => '$1$qS3ji685$KFJU/DfSDot1HpX69/fmf0',
+    'role' => 'user',
+    'email' => '2605111266@qq.com',
+  ),
+  26 => 
+  array (
+    'name' => 'Siyuan',
+    'pass' => '$1$tAc3mgYU$FvJSQhxiz/awZv39Djm3R.',
+    'role' => 'user',
+    'email' => '294873684@qq.com',
+  ),
+  27 => 
+  array (
+    'name' => 'mikeyu',
+    'pass' => '$1$2oUHG9wU$m79QcBG5LsShqBxFPC9VT.',
+    'role' => 'user',
+    'email' => '1746368076@qq.com',
+  ),
+  28 => 
+  array (
+    'name' => 'gjq',
+    'pass' => '$1$wtH9a2Yd$waRpEolp0JVRP6AUcpEu90',
+    'role' => 'user',
+  ),
+  29 => 
+  array (
+    'name' => 'BabyBenzene',
+    'pass' => '$1$bO1Cjdy7$bNdF4o.1vDQ.v4mrYpROt0',
+    'role' => 'user',
+  ),
+  30 => 
+  array (
+    'name' => 'usertest1',
+    'pass' => '$1$AIHraGWu$DcKWdc0RJ9MvmY.wIUVDr1',
+    'role' => 'user',
+  ),
+  31 => 
+  array (
+    'name' => 'zhuxy',
+    'pass' => '$1$JKuvUL7k$DZ1P9aItRC259irPnfXc30',
+    'role' => 'user',
+  ),
+  32 => 
+  array (
+    'name' => 'YuanHehuan_0014',
+    'pass' => '$1$nbO67qMn$LSbQayW5OxL4VM.3H44Bf/',
+    'role' => 'user',
+  ),
+  33 => 
+  array (
+    'name' => '又双叒叕',
+    'pass' => '$1$5Eeah8lq$36WmZFLnmCkJsu75VZpYV1',
+    'role' => 'admin',
+  ),
+  34 => 
+  array (
+    'name' => '江淮',
+    'pass' => '$1$uQJ8vgas$dP2BflebO4jM8ZPWENJZY1',
+    'role' => 'user',
+    'email' => '2933386552@qq.com',
+  ),
+);
