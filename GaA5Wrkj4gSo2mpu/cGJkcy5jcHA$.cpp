fa5905093cd76d10509c63831d45556f064f9904
vmlankub <45290401+vmlankub@users.noreply.github.com>
2020-02-14 20:22:49 +0800

    Original path: C:/Users/joefa/Desktop/WorkSpace/lab/_tmp_4/pbds.cpp

---
+++ GaA5Wrkj4gSo2mpu/cGJkcy5jcHA$.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+#define fi first
+#define se second
+#define em emplace
+#define eb emplace_back
+#define lb lower_bound
+#define ub upped_bound
+#define fbo find_by_order
+#define ofk order_of_key
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+template <class T>
+using pq = __gnu_pbds::priority_queue<T, greater<T>, pairing_heap_tag>;
+template <class T>
+using tr = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
