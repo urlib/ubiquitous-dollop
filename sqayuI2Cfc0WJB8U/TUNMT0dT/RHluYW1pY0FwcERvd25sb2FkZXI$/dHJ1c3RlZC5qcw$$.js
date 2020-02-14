2cb05b4debd7601b95d60a63cfc85c6c5a295075
vmlankub <45290401+vmlankub@users.noreply.github.com>
2020-02-14 22:41:15 +0800

    Original path: C:/ProgramData/McAfee/MCLOGS/DynamicAppDownloader/trusted.js

---
+++ sqayuI2Cfc0WJB8U/TUNMT0dT/RHluYW1pY0FwcERvd25sb2FkZXI$/dHJ1c3RlZC5qcw$$.js
@@ -0,0 +1,104 @@
+/*
+
+// Data version number. Increase it when a new cert is added. Make sure APICore _g_dwDataVersion has the same value.
+[VER]
+5
+
+// Trusted organizations. Make sure APICore _g_wszCompanyNames has the same values.
+[ORG]
+Intel Corporation
+McAfee, Inc.
+//Test Signing for Certificate Refresh Story
+
+// Trusted root CA hash. Make sure APICore _g_ppbyTrustedRootArray has the same values.
+[SHA]
+afe5d244a8d1194230ff479fe2f897bbcd7a8cb4
+02faf3e291435468607857694df5e45b68851868
+742c3192e607e424eb4549542be1bbc53e6174e2
+4eb6d578499b1ccf5f581ead56be3d9b6744a5e5
+
+// our test root ca
+//7e3d32486d6ca1dee638c33d5722effbc326517c
+//872ed3b336bf01648fe1d8bb50a8eb78039b881c
+
+// Revoked Serial Number list. Make sure APICore _g_wszSerialNumbers has the same values.
+[SER]
+// from mcinsctl.dll, enable this line for testing
+//7fd36ae018f52fc27b037f93a1a793d6
+
+*/
+
+
+// SIG // Begin signature block
+// SIG // MIIJJAYJKoZIhvcNAQcCoIIJFTCCCRECAQExCzAJBgUr
+// SIG // DgMCGgUAMGcGCisGAQQBgjcCAQSgWTBXMDIGCisGAQQB
+// SIG // gjcCAR4wJAIBAQQQEODJBs441BGiowAQS9NQkAIBAAIB
+// SIG // AAIBAAIBAAIBADAhMAkGBSsOAwIaBQAEFMoThhKgQRFI
+// SIG // jfm6Y484lvu27y0EoIIFozCCBZ8wggOHoAMCAQICCQCj
+// SIG // Nk0hlThkODANBgkqhkiG9w0BAQsFADBlMQswCQYDVQQG
+// SIG // EwJDQTEQMA4GA1UECAwHT250YXJpbzERMA8GA1UEBwwI
+// SIG // V2F0ZXJsb28xMTAvBgNVBAoMKEludGVsIFNlY3VyaXR5
+// SIG // IEdyb3VwIENvbnN1bWVyIFRydXN0IExpc3QwIBcNMTUw
+// SIG // OTAzMjEyMzAyWhgPMjExNTA4MTAyMTIzMDJaMGUxCzAJ
+// SIG // BgNVBAYTAkNBMRAwDgYDVQQIDAdPbnRhcmlvMREwDwYD
+// SIG // VQQHDAhXYXRlcmxvbzExMC8GA1UECgwoSW50ZWwgU2Vj
+// SIG // dXJpdHkgR3JvdXAgQ29uc3VtZXIgVHJ1c3QgTGlzdDCC
+// SIG // AiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBALmz
+// SIG // J+7ITmJt6bx0o7f9QIHVNDIwgUGdPIQDPxaZ5CkYWVck
+// SIG // A5hwHnueJL8uA/dkD6OYEkOQVaHQPt8iMn+WMxr477Ez
+// SIG // EyveRj1GDfH7tbUX8ifNMVcHcpJZrHC4kqGsQZBESiNI
+// SIG // KdW8EIpUltp5+MrFBb+Fm3jph/EaXWlrI7A4wi/4Xh5W
+// SIG // B5b8XAgpWknbeAaOuhH2MM35Q1q4ZQDxRkBABnNF2UXO
+// SIG // P8ScRM8Qp26uZvrykZ4LomR6YnKl26YpZg9hdGVZn41K
+// SIG // uIWyJV05Em3iOkqo2o3GWHPNrR1YVvqwq57EW6OgkZA+
+// SIG // FZIn68XrHfiiAKUzL7ZNr4WwqEpd7B84ehBSb8iYD1mN
+// SIG // L1VZyS5lI4S1+Q27yVD91DXNsbE+Fh871MMXmYqSekwR
+// SIG // ZKkILrdnHSjrdWehQ9AYVWuPJfr1z9IKOLyHluqirMdC
+// SIG // QNV6NQ+z3MZgZebxXouBT03eGnvkbbKFC4UHqf+WPnz1
+// SIG // QPSKNCCp1Ji/pBMKXURuJAxWutbbw7xK6YtUz9oD0PRW
+// SIG // qLYf6IZnv5ubN20qkVtivbwoufVXiLewNXZKzljK5OAl
+// SIG // K0V5FO+tS0eVu/h6eP+UxXlubXK1Xs7CQRX8yNjjXJU+
+// SIG // 2IKBpdXYeGRK2ym6RLanMLapP5Qfqn82GvgDLCIfElgD
+// SIG // S3swcdw018PBzFlbXfYTAgMBAAGjUDBOMB0GA1UdDgQW
+// SIG // BBRmiye4nwUkXMpfUlEB0zR/5hmaZDAfBgNVHSMEGDAW
+// SIG // gBRmiye4nwUkXMpfUlEB0zR/5hmaZDAMBgNVHRMEBTAD
+// SIG // AQH/MA0GCSqGSIb3DQEBCwUAA4ICAQAQigJOeO6sgSB4
+// SIG // 0gS8yH6HWQnphCIOrQm5OYUFccKPxGBcvNL2QEVAoz3D
+// SIG // KPzGvxP/O/EJzaHVjncoRw68tvMDnkwqts0VGEBHTuIa
+// SIG // o67w39l1Qtrl9Y9FyrlyQH/zSpMUQAlKrx2m5hDADegJ
+// SIG // 4znIqVRz+4kpyIpyvkAE4gJrz1IXwWJjpRzYcvfxHZXG
+// SIG // XR3yMHkaJ+6Hr2gE2nJrRYKwVC5IK6A5vXCbGi+FNHvk
+// SIG // ghwBpn7mkrXOSRY+kgYxUq27DWUJSmHJrDWaj+5eBfqv
+// SIG // jaQDJtYgt0gy1I9hh8PXtftYY/Vjau7R8L9QDPBH3Gn3
+// SIG // QWOlcFX1aTxn54AYbWe871gHyq5fDIJGwRlw+D3lX7f7
+// SIG // 99YOzjmLlSRJbSeRNV+khXOr/I6zHfhL+1MIJWaJDcD2
+// SIG // uJAVA2yu6hrXvKAhhZoudg1KZEAEAL2pV0q1TFb+hKl0
+// SIG // S/BktueiIxY6xJLmMvFxZH55Y+/RenPUO5FJehVlljIh
+// SIG // NCTANDzm5btKevvdxD9A0y4EZg4angDm7HkizC4lQH/8
+// SIG // qsZoB3AtEDArDIQWTWgcOQIvZgeg2Kfx+W7Wi4ae0VfI
+// SIG // HU6SaUshTrTxh9AptqQUzT9/ASVOUX5slYjLLkrw8wRI
+// SIG // ttn3rLgp6Zf51zsczKveRJezKd2cMEn7a8qaaf5qmjhh
+// SIG // WywRX5XiYjGCAu0wggLpAgEBMHIwZTELMAkGA1UEBhMC
+// SIG // Q0ExEDAOBgNVBAgMB09udGFyaW8xETAPBgNVBAcMCFdh
+// SIG // dGVybG9vMTEwLwYDVQQKDChJbnRlbCBTZWN1cml0eSBH
+// SIG // cm91cCBDb25zdW1lciBUcnVzdCBMaXN0AgkAozZNIZU4
+// SIG // ZDgwCQYFKw4DAhoFAKBSMBAGCisGAQQBgjcCAQwxAjAA
+// SIG // MBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMCMGCSqG
+// SIG // SIb3DQEJBDEWBBRbj7sWPFuHGBmm8a1UZGhPlBg4KDAN
+// SIG // BgkqhkiG9w0BAQEFAASCAgAYJfHe0ufyHVUxa0eVvgLg
+// SIG // Li2LRgTLjtjC3beSuW7MruC1nlqX6Wo9psPjwDQTDWx+
+// SIG // cAlTmx52DuRNspejpT/AKu6GVVn27Y/cTSl6wIkyqtKr
+// SIG // h5wFmhtZ49gDpUGTmg/l7phcM72tL9Yw9VoqAUBg8GaJ
+// SIG // 06HQXMTrI45cdcWuqqwGkr82tCoBlM0oTO0NuTnbMD+J
+// SIG // FjCksRAiSueBzxqncGmBcP01diaUcsou5yMr0obQ5NcX
+// SIG // 53Nf847MVVsbgnGeQ0+GJl56wsfNPRr+gq+gf/Z+q8F6
+// SIG // aXp/z5KYDRbzh09klSMGBENpLOOfrsURqWn3bTpMN26O
+// SIG // sbQ3vFhvZitvNe9lFT/sesYuyXIZ8+kmwLgoJ3oj9h2v
+// SIG // g4ajMt7yhlD7INw/cK+YcxeZ9m0HYuMfFdCvyAmNjBG5
+// SIG // 1+rjqECFlzv0ao/5qywdtHnFTm7ABw+QuZJO8JYkSrQQ
+// SIG // 0+f8rKxHiM8fS4F97hJXX/e28YBtuGvNI91dVsLtxHof
+// SIG // YJs1gng4cVPOlE/p4CBRXYHXfCyeq1c/mhaLj857JUvM
+// SIG // Yitld0phWG5tIhJCHMTFzzjA/FH9/lJAukH4F0350g9H
+// SIG // /7cki/tEvRraOebmoha8/5zDQYFjbvr4mxeA7GlMyN0W
+// SIG // s0jHJQbIzD9uM/72/KPKouStRDwkUpNLiyF86XSbgVgcZw==
+// SIG // End signature block
