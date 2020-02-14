cca83e6026e6b327d631b07881aee959ca0396c6
vmlankub <45290401+vmlankub@users.noreply.github.com>
2020-02-14 20:25:21 +0800

    Original path: C:/Users/joefa/Desktop/WorkSpace/lab/_tmp_4/double.cpp

---
+++ JiYCns02kATF9Vwr/ZG91YmxlLmNwcA$$.cpp
@@ -0,0 +1,13 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+double a[100];
+pair<double, double> b[100];
+
+int main()
+{
+    memset(a, 0, sizeof(a));
+    memset(b, 0, sizeof(b));
+
+    cout << a[0] << b[0].first << b[0].second << endl;
+}
