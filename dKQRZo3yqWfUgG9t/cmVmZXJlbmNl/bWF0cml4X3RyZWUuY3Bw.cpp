552cb7811e48ed4fcbddb65b5bfbfad13a761489
vmlankub <45290401+vmlankub@users.noreply.github.com>
2020-02-14 20:41:26 +0800

    Original path: C:/Users/joefa/Desktop/WorkSpace/517/problem-solving-master/reference/matrix_tree.cpp

---
+++ dKQRZo3yqWfUgG9t/cmVmZXJlbmNl/bWF0cml4X3RyZWUuY3Bw.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define zero(x) ((x>0? x:-x)<1e-15)
+const int N = 100;
+double a[N][N];
+double b[N][N];
+int g[110][110];
+double det(double a[][N],int n) {
+    int i, j, k, sign = 0;
+    double ret = 1, t;
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            b[i][j] = a[i][j];
+    for (i = 0; i < n; i++) {
+        if (zero(b[i][i])) {
+            for (j = i + 1; j < n; j++)
+                if (!zero(b[j][i]))
+                    break;
+            if (j == n)
+                return 0;
+            for (k = i; k < n; k++)
+                swap(b[i][k], b[j][k]);
+            sign++;
+        }
+        ret *= b[i][i];
+        for (k = i + 1; k < n; k++)
+            b[i][k] /= b[i][i];
+        for (j = i + 1; j < n; j++)
+            for (k = i + 1; k < n; k++)
+                b[j][k] -= b[j][i] * b[i][k];
+    }
+    if (sign & 1)
+        ret = -ret;
+    return ret;
+}
+int main() {
+    memset(a, 0, sizeof(a));
+    for (int i = 0; i < 4; i++) {
+        a[i][i] = 3;
+        for (int j = 0; j < 4; j++) if(j != i){
+            a[i][j] --;
+        }
+    }
+    cout << det(a, 3);
+    /* 0 based
+     * a[i][i] = degree[i];
+     * if(g[i][j]) {
+     *     a[i][j]--;
+     * }
+     * ans = det(a, n - 1);
+     */
+}
+
